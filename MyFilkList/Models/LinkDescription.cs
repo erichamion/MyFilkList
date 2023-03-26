3e7b31a4f54895cbb2e1e0bf0445eaf8f294438d
Eric Ray <erichamion@gmail.com>
2023-03-26 00:28:21 -0500

    minor updates

--- MyFilkList/Models/LinkDescription.cs
+++ MyFilkList/Models/LinkDescription.cs
@@ -2,6 +2,6 @@ namespace MyFilkList.Models;
 
 public class LinkDescription
 {
-    public string Text { get; set; }
-    public Uri Url { get; set; }
+    public string? Text { get; set; }
+    public Uri? Url { get; set; }
 }
